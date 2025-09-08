e8774de6fe094d34526fa3692302432e6e520daa
TIGER <suraj23082002@gmail.com>
2025-09-08 14:25:37 +0530

    added day 48

---
+++ day-48/day-48-panic.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Start of Program 🚀")
+
+	// Panic call
+	panic("Something went wrong ❌")
+
+	// Ye line kabhi execute nahi hogi
+	fmt.Println("End of Program")
+}
