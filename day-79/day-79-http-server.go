7d35467f0272fba25624ed46bdb70a5f815e8126
TIGER <suraj23082002@gmail.com>
2025-10-15 13:36:12 +0530

    added day 79

---
+++ day-79/day-79-http-server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Tiger 🐯, Welcome to Go HTTP Server!")
+}
+
+func main() {
+	// Register handler function
+	http.HandleFunc("/", helloHandler)
+
+	// Start server on port 8080
+	fmt.Println("Server running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
